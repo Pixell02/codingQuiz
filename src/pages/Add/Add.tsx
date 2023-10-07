dbc29fabcafed440b9ca831dfe57df31c43bdb6f
Pixel <nikodems1308@gmail.com>
2023-10-07 15:39:38 +0200

    reduce code, to custom hook

--- src/pages/Add/Add.tsx
+++ src/pages/Add/Add.tsx
@@ -1,41 +1,31 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../../components/Button";
 import Input from "../../../components/Input";
-
-interface answerProps {
-  answer: string;
-  isTrue: boolean
-}
-interface taskProps {
-  language: string;
-  level: string;
-  question: string;
-  answers: answerProps[]
-}
+import useTask from "./hooks/useTask";
 
 const Add = () => {
   const navigate = useNavigate();
-  const [task, setTask] = useState<taskProps>({
-    language: "",
-    level: "",
-    question: "",
-    answers: []
-  })
+  const { task, handleClickInput, handleChangeAnswer } = useTask();
+
   const handleAddClick = () => {
     navigate("/");
   };
-  const handleClickInput = (value: string, className: string) => {
-    setTask((prev) => ({
-      ...prev,
-      [className]: value
-    }))
-  }
 
   return (
     <div className="settings-window-container">
       <div className="w-75">
-        <Input value={task.question} className="question" onChange={handleClickInput} />
+        <Input value={task.question} name="Pytanie" className="question" onChange={handleClickInput} />
+
+        {task.answers.map((answer, i) => (
+          <div key={i}>
+            <Input
+              value={answer.answer}
+              name={`Odpowiedź ${i === 0 ? "dobra" : "zła"}`}
+              className={`answer-${i}`}
+              onChange={(value) => handleChangeAnswer(value, i)}
+            />
+          </div>
+        ))}
         <Button name="Dodaj" onClick={handleAddClick} />
       </div>
     </div>
