769d08b265b9e1fef2bce2d388e4d1c23a5ac545
Pixel <nikodems1308@gmail.com>
2023-10-07 18:31:03 +0200

    add boolean

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,11 +3,12 @@ import "./Button.css";
 interface props {
     name: string
     onClick: () => void;
+    blocked?: boolean
 }
 
-const Button = ({name, onClick}: props) => {
+const Button = ({name, onClick, blocked}: props) => {
   return (
-    <button className='rounded-button' onClick={onClick}>
+    <button className='rounded-button' onClick={onClick} disabled={blocked} >
         {name}
     </button>
   )
