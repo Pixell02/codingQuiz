e7cc2e3f1831a833305eaab99a4af78408afa24f
Pixel <nikodems1308@gmail.com>
2023-10-07 13:23:01 +0200

    create page component

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,8 +1,10 @@
+import SettingWindow from "./components/SettingWindow"
 
 const Main = () => {
   return (
-    <div>
-      <p>asdasasdasdd</p>
+    <div className="d-flex w-100 align-items-center justify-content-center flex-column">
+      <h1>Coding quiz</h1>
+      <SettingWindow />
     </div>
   )
 }
