901b36fa2007c810cb71838ba984ec7e32a8d158
Pixel <nikodems1308@gmail.com>
2023-10-07 15:39:24 +0200

    change build of input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,15 +5,22 @@ interface InputProps {
   value: string;
   onChange: (value: string, className: string) => void;
   className: string;
+  name?:string
 }
 
-const Input = ({ value, onChange, className }: InputProps) => {
+const Input = ({ value, onChange, className, name }: InputProps) => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     onChange(value, className);
   };
 
-  return <input type="text" value={value} className={className} onChange={handleChange} />;
+  return (
+  <div className="input-container">
+    <span className="input-title">{name}</span>
+    <input type="text" value={value} className={className} onChange={handleChange} />
+  </div>
+  );
+
 };
 
 export default Input;
