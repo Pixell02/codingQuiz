593d02d27167e32b5e13b6053033946b17fee343
Pixel <nikodems1308@gmail.com>
2023-10-07 23:42:16 +0200

    add components

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,12 +1,13 @@
-import Couter from "./components/Couter";
+import Counter from "./components/Counter";
+import Window from "./components/Window";
 import { QuestionProvider } from "./context/QuestionContext";
 
 const Quiz = () => {
   return (
     <QuestionProvider>
       <div className="d-flex flex-column justify-content-center align-items-center">
-      <Couter />
-      <div className="settings-window-container"></div>
+      <Counter />
+      <Window />
       </div>
     </QuestionProvider>
   )
