8fb452ee188a59c3fd442521cd93f4458d5b2d76
Pixel <nikodems1308@gmail.com>
2023-10-07 13:53:19 +0200

    add types, add navigation, add state

--- src/pages/Add/Add.tsx
+++ src/pages/Add/Add.tsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../../components/Button";
+import Input from "../../../components/Input";
 
-const Add = () => {
-  return (
-    <div className="settings-window-container">
-      
-    </div>
-  )
+interface answerProps {
+  answer: string;
+  isTrue: boolean
+}
+interface taskProps {
+  language: string;
+  level: string;
+  question: string;
+  answers: answerProps[]
 }
 
-export default Add
+const Add = () => {
+  const navigate = useNavigate();
+  const [task, setTask] = useState<taskProps>({
+    language: "",
+    level: "",
+    question: "",
+    answers: []
+  })
+  const handleAddClick = () => {
+    navigate("/");
+  };
+  const handleClickInput = (value: string, className: string) => {
+    setTask((prev) => ({
+      ...prev,
+      [className]: value
+    }))
+  }
+
+  return (
+    <div className="settings-window-container">
+      <div className="w-75">
+        <Input value={task.question} className="question" onChange={handleClickInput} />
+        <Button name="Dodaj" onClick={handleAddClick} />
+      </div>
+    </div>
+  );
+};
+
+export default Add;
