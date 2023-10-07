834dca14f10ed5704cf86dce03ef159723fa4bd5
Pixel <nikodems1308@gmail.com>
2023-10-07 13:30:57 +0200

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Add from "./pages/Add/Add";
 import Main from "./pages/Main/Main";
+import Quiz from "./pages/Quiz/Quiz";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/add" element={<Add/>} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </BrowserRouter>
   );
